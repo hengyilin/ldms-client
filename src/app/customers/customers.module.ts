29f0bdbca18f7028f037a3f147c868478e35465d
hengyilin <hylin601@126.com>
2019-06-22 16:29:44 +0800

    angular学习——Card

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,14 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { CustomersRoutingModule } from './customers-routing.module';
-import { CustomerListComponent } from './customer-list/customer-list.component';
+import {CustomersRoutingModule} from './customers-routing.module';
+import {CustomerListComponent} from './customer-list/customer-list.component';
+import {CustomerCardComponent} from './customer-card/customer-card.component';
+
+// Material
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+
 
 @NgModule({
-  declarations: [CustomerListComponent],
+  declarations: [CustomerListComponent, CustomerCardComponent],
   imports: [
     CommonModule,
-    CustomersRoutingModule
+    CustomersRoutingModule,
+
+    //Material
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule
   ]
 })
-export class CustomersModule { }
+export class CustomersModule {
+}
