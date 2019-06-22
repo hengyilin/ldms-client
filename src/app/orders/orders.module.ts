95bc876e9fdfc2d2a7b27bd2bf12421ae5740d56
hengyilin <hylin601@126.com>
2019-06-22 18:52:46 +0800

    angular学习——Date

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,21 +1,36 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { OrdersRoutingModule } from './orders-routing.module';
-import { OrderListComponent } from './order-list/order-list.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {OrderListComponent} from './order-list/order-list.component';
+import {OrderNewComponent} from './order-new/order-new.component';
+import {OrdersRoutingModule} from './orders-routing.module';
 
 // Material
-import {MatTabsModule} from "@angular/material";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatMomentDateModule} from "@angular/material-moment-adapter";
 
 @NgModule({
-  declarations: [OrderListComponent],
+  declarations: [OrderListComponent, OrderNewComponent],
   imports: [
     CommonModule,
     OrdersRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
 
     // Material
-    MatTabsModule
-
+    MatTabsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatInputModule,
+    MatMomentDateModule
   ]
 })
-export class OrdersModule { }
+export class OrdersModule {
+}
